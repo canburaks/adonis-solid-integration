9fae7e22c02e48844af7ebd22a225d6a9df41659
Can Burak S <canburak@msn.com>
2021-10-22 21:42:51 +0300

    ..

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+import "../css/app.css";
+import { createSignal, onCleanup } from "solid-js";
+import { render } from "solid-js/web";
+
+const App = () => {
+  const [count, setCount] = createSignal(0),
+    timer = setInterval(() => setCount(count() + 1), 1000);
+
+  onCleanup(() => clearInterval(timer));
+
+  const style = `
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    font-size:128px;
+    font-weight:bold;
+    height:100vh;
+  `;
+  return <div style={style}>{count()}</div>;
+};
+
+render(() => <App />, document.getElementById("app"));
